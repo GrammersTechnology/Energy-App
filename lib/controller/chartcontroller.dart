0967a53dae50851d83670902ee728798874013a3
shabanas <shabanaspb@gmail.com>
2023-06-27 18:24:20 +0530

    chart screen added

---
+++ lib/controller/chartcontroller.dart
@@ -0,0 +1,115 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../services/chart_services.dart';
+
+class ChartController extends ChangeNotifier {
+  String? dropdowmValue;
+  ChartDataModel? data;
+  bool loader = false;
+  List dropdwonList = ["NO1", "NO2", "NO3", "NO4", "NO5"];
+  List result = [];
+  changeDropDownValue(value) {
+    dropdowmValue = value;
+    notifyListeners();
+    setChartData(result);
+  }
+
+  getChartDetails(context, String zone) async {
+    loader = true;
+    result = (await ChartService().chartGraphDataApi(context))!;
+    if (result.isNotEmpty) {
+      setChartData(result);
+    }
+    loader = false;
+  }
+
+  setChartData(List result) async {
+    loader = true;
+    if (dropdowmValue != null) {
+      final response =
+          dropdowmValue.toString().substring(dropdowmValue!.length - 1);
+      for (var element in result) {
+        if (element['omrType'].toString() == 'EL') {
+          if (element['omrnr'].toString() == response) {
+            log("omnr ${element['omrnr'].toString()}");
+            log(element['fyllingsgrad'].toString());
+            log(element['kapasitet_TWh'].toString());
+            log(element['fylling_TWh'].toString());
+            log(element['fyllingsgrad_forrige_uke'].toString());
+            log(element['endring_fyllingsgrad'].toString());
+            data = ChartDataModel(
+                omrnr: element['omrnr'],
+                isoAar: element["iso_aar"],
+                isoUke: element['iso_uke'],
+                fyllingsgrad: element['fyllingsgrad'],
+                kapasitetTWh: element['kapasitet_TWh'],
+                fyllingTWh: element['fylling_TWh'],
+                fyllingsgradForrigeUke: element['fyllingsgrad_forrige_uke'],
+                endringFyllingsgrad: element['endring_fyllingsgrad']);
+          }
+        }
+      }
+    } else {
+      final perf = await SharedPreferences.getInstance();
+
+      final zone = perf.getString("zone");
+
+      final response = zone.toString().substring(zone!.length - 1);
+      log(response.toString());
+      for (var element in result) {
+        if (element['omrType'].toString() == 'EL') {
+          if (element['omrnr'].toString() == response) {
+            log("omnr ${element['omrnr'].toString()}");
+            log(element['fyllingsgrad'].toString());
+            log(element['kapasitet_TWh'].toString());
+            log(element['fylling_TWh'].toString());
+            log(element['fyllingsgrad_forrige_uke'].toString());
+            log(element['endring_fyllingsgrad'].toString());
+            data = ChartDataModel(
+                omrnr: element['omrnr'],
+                isoAar: element["iso_aar"],
+                isoUke: element['iso_uke'],
+                fyllingsgrad: element['fyllingsgrad'],
+                kapasitetTWh: element['kapasitet_TWh'],
+                fyllingTWh: element['fylling_TWh'],
+                fyllingsgradForrigeUke: element['fyllingsgrad_forrige_uke'],
+                endringFyllingsgrad: element['endring_fyllingsgrad']);
+          }
+        }
+      }
+    }
+    loader = false;
+    notifyListeners();
+  }
+}
+
+class ChartDataModel {
+  DateTime? datoId;
+  String? omrType;
+  int? omrnr;
+  int? isoAar;
+  int? isoUke;
+  double? fyllingsgrad;
+  double? kapasitetTWh;
+  double? fyllingTWh;
+  DateTime? nestePubliseringsdato;
+  double? fyllingsgradForrigeUke;
+  double? endringFyllingsgrad;
+
+  ChartDataModel({
+    this.datoId,
+    this.omrType,
+    this.omrnr,
+    this.isoAar,
+    this.isoUke,
+    this.fyllingsgrad,
+    this.kapasitetTWh,
+    this.fyllingTWh,
+    this.nestePubliseringsdato,
+    this.fyllingsgradForrigeUke,
+    this.endringFyllingsgrad,
+  });
+}
---
+++ lib/services/chart_services.dart
@@ -0,0 +1,32 @@
+import 'package:dio/dio.dart';
+import 'package:provider/provider.dart';
+
+import '../const/api_error_helper.dart';
+import '../controller/chartcontroller.dart';
+
+class ChartService {
+  Future<List<dynamic>?> chartGraphDataApi(context) async {
+    final chartController =
+        Provider.of<ChartController>(context, listen: false);
+    chartController.loader = true;
+    String url =
+        'https://biapi.nve.no/magasinstatistikk/api/Magasinstatistikk/HentOffentligDataSisteUke';
+
+    try {
+      Response response = await Dio().get(url);
+      if (response.statusCode == 200) {
+        final result = response.data;
+        chartController.loader = false;
+
+        chartController.notifyListeners();
+        return result;
+      }
+    } on DioException catch (e) {
+      chartController.loader = false;
+      chartController.notifyListeners();
+      ErrorHandlerCode().status401(e);
+      // print(e);
+    }
+    return null;
+  }
+}
